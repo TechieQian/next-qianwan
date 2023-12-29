8265d51f7c67d8de1c363dd39b14d6acaeec5499
Qian <qiansan@gmail.com>
2023-12-29 15:21:18 -0500

    upgrade

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,10 @@ import React from "react";
 const IndexPage = () => (
   <article>
     <b>Hello!</b> My name is Qian and I am a software engineer with over 12
-    years experience in fintech. I began creating webpages during the geocities
-    days and had the pain and pleasure of coding through the eras. In college I
-    majored in computer engineering and obsessed over FPGA and C++. But I
-    eventually found my way back home in webdev and lean towards Front-End. I
-    enjoy interface-driven architecture whether it be a React component or a
-    protobuff file. Simplicity is complicated. Talk to me about React, browser
-    engines, and reasons we don't have gRPC streaming over web yet.
+    years of experience building web, primarily in fintech. I began creating webpages during the geocities
+    days and had the pain and pleasure of coding through the eras. In college I obsessed over FPGA and C++. But I
+    eventually found my way back home in webdev. I love the bleeding edge of what web can do, and look forward to
+    the day when we can have native animation APIs, gRPC streaming, and runtime es6 imports. Hit me up if I missed the boat.
   </article>
 );
 
