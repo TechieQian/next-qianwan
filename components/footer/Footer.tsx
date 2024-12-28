9b5621f5922f3abe111ec14fa03968ec8eb52bbc
Qian Wan <qianwan@Qians-MacBook-Air.local>
2024-12-28 12:25:47 -0500

    2025

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -3,12 +3,12 @@ import * as React from "react";
 
 export default function Footer() {
   return (
-    <footer className="footer">
-      <div className="footer__links">
+    <footer className="q-footer">
+      <div className="q-footer__links">
         <Link href="/">About</Link>
         <Link href="/resume/">Resume</Link>
       </div>
-      <p className="footer__copyright">© 2024 by qian</p>
+      <p className="q-footer__copyright">© 2025 by qian</p>
     </footer>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,16 @@ import React from "react";
 
 const IndexPage = () => (
   <article>
-    <b>Hello!</b> My name is Qian and I am a software engineer with over 12
-    years of experience building web, primarily in fintech. I began creating webpages during the geocities
-    days and had the pain and pleasure of coding through the eras. In college I obsessed over FPGA and C++. But I
-    eventually found my way back home in webdev. I love the bleeding edge of what web can do, and look forward to
-    the day when we can have native animation APIs, gRPC streaming, and runtime es6 imports. Hit me up if I missed the boat.
+    <b>Hello!</b> My name is Qian and I am a software engineer with over 15
+    years of experience building web application at the intersection of finance and tech.
+    <p style={{ marginTop: "1em" }}>
+      I lead the development of LinkedIn's first AI feature, editorial tooling, and
+      also developed LinkedIn's games platform from scratch (I know right).
+    </p>
+    <p style={{ marginTop: "1em"}}>
+      At financial institutions such as Blackstone and King Street, I helped build the next generation of web stack.
+      I am currently a software engineer at Jain Global, building the entire suite of web applications for the biggest hedge fund launch.
+    </p>
   </article>
 );
 
